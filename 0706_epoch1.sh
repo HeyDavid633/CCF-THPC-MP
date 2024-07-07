b5fbe041b908bcc16c0a41406b8fb9d78afaef36
HeyDavid633 <2019011758@student.cup.edu.cn>
2024-07-07 09:56:10 +0800

    restore A100 performance data, ready for GNN / GAN net

--- 0706_epoch1.sh
+++ 0706_epoch1.sh
@@ -1,6 +1,12 @@
-python train_cv.py -net alexnet -epoch 5 -precision fp32
-python train_cv.py -net alexnet -epoch 5 -precision amp
 python train_cv_emp_time.py -net alexnet -epoch 5 -precision emp
-python train_cv.py -net vgg16 -epoch 5 -precision fp32 -batch_size 128
-python train_cv.py -net vgg16 -epoch 5 -precision amp -batch_size 128
-python train_cv_emp_time.py -net vgg16 -epoch 5 -precision emp -batch_size 128
+python train_cv.py -net alexnet -epoch 5 -precision amp 
+python train_cv.py -net alexnet -epoch 5 -precision fp32
+python train_cv_emp_time.py -net vgg16 -epoch 5 -precision emp
+python train_cv.py -net vgg16 -epoch 5 -precision amp
+python train_cv.py -net vgg16 -epoch 5 -precision fp32
+python train_cv_imagenet_emp_time.py -net alexnet -epoch 1 -precision emp
+python train_cv_imagenet.py -net alexnet -epoch 1 -precision amp
+python train_cv_imagenet.py -net alexnet -epoch 1 -precision fp32
+python train_cv_imagenet_emp_time.py -net vgg16 -epoch 1 -precision emp -batch_size 128
+python train_cv_imagenet.py -net vgg16 -epoch 1 -precision amp -batch_size 128
+python train_cv_imagenet.py -net vgg16 -epoch 1 -precision fp32 -batch_size 128
